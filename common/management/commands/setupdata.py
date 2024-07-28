db3c405252b0d65d97552dc5f78191ea670fe7b2
VadKhar <VadKhar@outlook.com>
2024-07-28 17:58:28 +0300

    Add `migrate` and `createsuperuser` to `setupdata`

--- common/management/commands/setupdata.py
+++ common/management/commands/setupdata.py
@@ -1,22 +1,45 @@
+import os
+from secrets import token_urlsafe
+
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
 
 class Command(BaseCommand):
-    help = "Populate data base with initial data"
+    help = "Migrate and populate data base with initial data"
 
     def handle(self, *args, **options):
-        call_command('loaddata', 'country.json', verbosity=0)
-        call_command('loaddata', 'currency.json', verbosity=0)
-        call_command('loaddata', 'groups.json', verbosity=0)
-        call_command('loaddata', 'resolution.json', verbosity=0)
-        call_command('loaddata', 'department.json', verbosity=0)
-        call_command('loaddata', 'deal_stage.json', verbosity=0)
-        call_command('loaddata', 'projectstage.json', verbosity=0)
-        call_command('loaddata', 'taskstage.json', verbosity=0)
-        call_command('loaddata', 'client_type.json', verbosity=0)
-        call_command('loaddata', 'closing_reason.json', verbosity=0)
-        call_command('loaddata', 'industry.json', verbosity=0)
-        call_command('loaddata', 'lead_source.json', verbosity=0)
-        call_command('loaddata', 'publicemaildomain.json', verbosity=0)
-        call_command('loaddata', 'help_en.json', verbosity=0)
+        if not settings.TESTING:
+            call_command('migrate', verbosity=1)
+
+        call_command(
+            'loaddata',
+            'country.json',
+            'currency.json',
+            'groups.json',
+            'resolution.json',
+            'department.json',
+            'deal_stage.json',
+            'projectstage.json',
+            'taskstage.json',
+            'client_type.json',
+            'closing_reason.json',
+            'industry.json',
+            'lead_source.json',
+            'publicemaildomain.json',
+            'help_en.json',
+            verbosity=1
+        )
+        if not settings.TESTING:
+            pas = token_urlsafe(6)
+            os.environ.setdefault('DJANGO_SUPERUSER_PASSWORD', pas)
+            os.environ.setdefault('DJANGO_SUPERUSER_USERNAME', 'IamSUPER')
+            os.environ.setdefault('DJANGO_SUPERUSER_EMAIL', 'super@example.com')
+            call_command('createsuperuser', '--noinput', verbosity=1)
+            print(
+                "SUPERUSER Credentials:\n",
+                " USERNAME: IamSUPER\n",
+                f" PASSWORD: {pas}\n",
+                " EMAIL: super@example.com\n"
+            )
