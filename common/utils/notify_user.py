024799cd6c024e55842236f7573c6bb8058510e0
VadKhar <VadKhar@outlook.com>
2024-08-14 08:54:25 +0300

    Fix parameter typing

--- common/utils/notify_user.py
+++ common/utils/notify_user.py
@@ -1,17 +1,17 @@
 from importlib import import_module
 from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth.models import User
 from django.core.handlers.wsgi import WSGIRequest
 
 from common.utils.helpers import notify_admins_no_email
 from common.utils.helpers import save_message
-from common.utils.helpers import USER_MODEL
 from common.utils.email_to_participants import email_to_participants
 
 SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
 
 
-def notify_user(obj, user: USER_MODEL, subject: str, message='', *,
+def notify_user(obj, user: User, subject: str, message='', *,
                 level='INFO', responsible=None, request: WSGIRequest = None) -> None:
     if request and request.user == user:
         messages.info(request, message)
