551357e1c1fa7203ddbf20adfa7e825b87aa9304
VadKhar <VadKhar@outlook.com>
2024-09-16 20:24:29 +0300

    Add `test_secure_url`

---
+++ tests/common/utils/test_secure_url.py
@@ -0,0 +1,30 @@
+from django.contrib.sites.models import Site
+from django.test import RequestFactory
+from django.test import TestCase
+from django.urls import reverse
+
+from common.utils.secure_url import secure_url
+
+
+class TestSecureUrl(TestCase):
+
+    def test_secure_url(self):
+        relative_url = reverse("site:crm_company_changelist")
+        factory = RequestFactory()
+        # Create an instance of a GET request.
+        request = factory.get(relative_url)
+        self.assertEqual(relative_url, secure_url(relative_url, request))
+        site = Site.objects.get_current()
+        https_url = f"https://{site.domain}{relative_url}"
+        self.assertEqual(https_url, secure_url(
+            https_url, factory.get(https_url)))
+
+        http_url = f"http://{site.domain}{relative_url}"
+        self.assertEqual(https_url, secure_url(
+            http_url, factory.get(https_url)))
+        
+        # alien domain
+        url = f"https://www.google.com{relative_url}"
+
+        self.assertEqual('/', secure_url(url, factory.get(url)))
+
