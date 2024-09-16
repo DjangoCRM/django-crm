d6030b8e2a2098a40390cc18564d817f845c9126
VadKhar <django.crm@outlook.com>
2024-09-16 16:42:27 +0300

    Create `secure_url` function

---
+++ common/utils/secure_url.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.core.handlers.wsgi import WSGIRequest
+from urllib.parse import urlparse
+
+
+def secure_url(url: str, request: WSGIRequest) -> str:
+    """
+    Checks the domain and scheme of an absolute url.
+    If the domain is alien, returns the URL of the index page.
+    """
+    scheme, netloc, _, _, _, _ = urlparse(url)
+    if netloc:
+        site = Site.objects.get_current()
+        domain = site.domain
+        if netloc != domain:
+            return '/'
+        
+        if scheme == 'http':
+            if settings.TESTING:
+                # the request factory always uses http
+                url = 'https' + url[4:]
+
+            else:
+                if scheme != request.scheme:
+                    url = 'https' + url[4:]
+
+    return url
+ 
