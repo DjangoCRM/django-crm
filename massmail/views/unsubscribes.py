7925a71bb402475f02ceae005a2723032884c3db
VadKhar <VadKhar@outlook.com>
2024-09-26 08:42:09 +0300

    Refactor unsubscribe logic with error handling.
    Updated the unsubscribe function to handle cases where the recipient does not exist. The changes ensure that the massmail flag is accurately set to false for both MassContact and its associated content object, enhancing data consistency.

--- massmail/views/unsubscribes.py
+++ massmail/views/unsubscribes.py
@@ -1,10 +1,21 @@
-from massmail.models import MassContact
-from django.http import HttpResponseRedirect
 from django.conf import settings
+from django.http import HttpResponseRedirect
+
+from massmail.models import MassContact
 
 
-def unsubscribe(request, recipient_uuid):
-    MassContact.objects.filter(
-        uuid=recipient_uuid
-    ).update(massmail=False)
+def unsubscribe(request, recipient_uuid) -> HttpResponseRedirect:
+    try:
+        mc = MassContact.objects.get(
+            uuid=recipient_uuid
+        )
+    except MassContact.DoesNotExist:
+        return HttpResponseRedirect(settings.UNSUBSCRIBE_URL)
+
+    mc.massmail=False
+    mc.save(update_fields=['massmail'])
+    counterparty = mc.content_object
+    counterparty.massmail = False
+    counterparty.save(update_fields=['massmail'])
+
     return HttpResponseRedirect(settings.UNSUBSCRIBE_URL)
--- tests/massmail/views/test_unsubscribe.py
+++ tests/massmail/views/test_unsubscribe.py
@@ -3,6 +3,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.test import tag
 from django.urls import reverse
 
+from crm.models.company import Company
+from crm.models.contact import Contact
 from massmail.models.mass_contact import MassContact
 from tests.base_test_classes import BaseTestCase
 
@@ -13,11 +15,14 @@ from tests.base_test_classes import BaseTestCase
 class TestUnsubscribe(BaseTestCase):
 
     def setUp(self):
-        print("Run Test Method:", self._testMethodName)
+        print(" Run Test Method:", self._testMethodName)
 
     def test_unsubscribe(self):
+        company = Company.objects.create()
+        contact = Contact.objects.create(company_id=company.id)
         content_type = ContentType.objects.get(app_label='crm', model='contact')
-        mc = MassContact.objects.create(content_type=content_type, object_id=1)
+        mc = MassContact.objects.create(
+            content_type=content_type, object_id=contact.id)
         url = reverse('unsubscribe', args=(mc.uuid,))
         home_url = reverse('site:index')
         with self.settings(UNSUBSCRIBE_URL=home_url):
@@ -29,3 +34,5 @@ class TestUnsubscribe(BaseTestCase):
             )
         mc.refresh_from_db()
         self.assertEqual(mc.massmail, False)
+        contact.refresh_from_db()
+        self.assertEqual(contact.massmail, False)
