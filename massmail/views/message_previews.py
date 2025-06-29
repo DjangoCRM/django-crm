433ac62ecc1c793dbc9c21fe842b4169dc7fc0a6
VadKhar <django.crm@outlook.com>
2025-06-29 11:06:50 +0000

    Add the URL of the page "you have unsubscribed successfully" to the context of the preview of messages for the mailing.

--- massmail/views/message_previews.py
+++ massmail/views/message_previews.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.template import Template
+
 from massmail.models import EmlMessage
-from django.template import RequestContext, Template
+from settings.models import MassmailSettings
 
 
 def message_preview(request, message_id):
@@ -8,18 +11,18 @@ def message_preview(request, message_id):
     message = EmlMessage.objects.get(id=message_id)
     signature = message.signature
     signature_content = signature.content if signature else ''
-    content = "{% load mailbuilder %}"
     content = f"""
-    {content} 
+    {{% load mailbuilder %}}
     SUBJECT: {message.subject}<br>
     {message.content}<br>
     {signature_content}
     """
+    unsubscribe_url = MassmailSettings.objects.get(pk=1).unsubscribe_url
     context = RequestContext(
         request,
         {
             'first_name': message.owner.username,
-            'unsubscribe_url': '',
+            'unsubscribe_url':unsubscribe_url,
             'preview': True
         }
     )
