1e3e894dcfe29eec2b0bf8033de9a01cdefdf7c8
VadKhar <django.crm@outlook.com>
2025-06-30 13:04:23 +0300

    Refactor `get_rendered_msg` to enforce type hinting and improve signature handling

--- massmail/utils/helpers.py
+++ massmail/utils/helpers.py
@@ -2,15 +2,20 @@ from django.template import Context
 from django.template import Template
 
 from common.utils.get_signature_preview import get_rendered_context
+from massmail.models.email_message import EmlMessage
 
 
-def get_rendered_msg(message):
+def get_rendered_msg(message: EmlMessage, show_signature: bool = True) -> str:
+    """
+    Render message content for a mailing with optional signature.
+    """
     content = f"""
     {{% load mailbuilder %}}
     SUBJECT: {message.subject}<br>
     {message.content}<br>
-    {message.signature.content if message.signature else ''}
+    {message.signature.content if message.signature and show_signature else ''}
     """
     template = Template(content)
     context = Context({'preview': True})
+    
     return get_rendered_context(template, context)
