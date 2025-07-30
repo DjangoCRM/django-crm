c1f3ca4f7d054e5c3cd186d59f7dedc1e57a0876
VadKhar <django.crm@outlook.com>
2025-07-30 15:49:09 +0300

    Update datetime formats for improved consistency across locales

--- webcrm/datetime_settings.py
+++ webcrm/datetime_settings.py
@@ -5,7 +5,10 @@ from django.conf.locale.en import formats as en_formats
 from django.conf.locale.es import formats as es_formats
 from django.conf.locale.es import formats as fr_formats
 from django.conf.locale.it import formats as it_formats
+from django.conf.locale.nl import formats as nl_formats
+from django.conf.locale.pl import formats as pl_formats
 from django.conf.locale.pt_BR import formats as pt_br_formats
+from django.conf.locale.ro import formats as ro_formats
 from django.conf.locale.ru import formats as ru_formats
 from django.conf.locale.tr import formats as tr_formats
 from django.conf.locale.uk import formats as uk_formats
@@ -15,35 +18,47 @@ from django.conf.locale.uk import formats as uk_formats
 # see https://docs.djangoproject.com/en/dev/ref/templates/builtins/#date
 
 de_formats.DATE_FORMAT = "j.m.y"            # 5.10.25
-de_formats.DATETIME_FORMAT = "d. m Y H:i"   # 05. 10 2025 14:30
+de_formats.DATETIME_FORMAT = "j. M Y H:i"   # 05. Okt 2025 14:30
 de_formats.SHORT_DATE_FORMAT = "d.m.y"      # 05.10.25
 
-en_formats.DATE_FORMAT = "m/d/y"            # 10/25/25
+en_formats.DATE_FORMAT = "m/d/y"            # 10/05/25
 en_formats.DATETIME_FORMAT = "m j, Y, P"    # Oct 5, 2025, 1:30 p.m.
-en_formats.SHORT_DATE_FORMAT = "m/d/y"      # 10/25/25
+en_formats.SHORT_DATE_FORMAT = "m/d/y"      # 10/05/25
 
 es_formats.DATE_FORMAT = "d/m/y"
 es_formats.DATETIME_FORMAT = r"j \d\e M \d\e Y à\s H:i"
 es_formats.SHORT_DATE_FORMAT = "d/m/y"
 
-fr_formats.DATE_FORMAT = "d/m/y"            # 25/10/25
+fr_formats.DATE_FORMAT = "d/m/y"            # 05/10/25
 fr_formats.DATETIME_FORMAT = r"j \d\e M \d\e Y \a \l\a\s H:i"
-fr_formats.SHORT_DATE_FORMAT = "d/m/y"      # 25/10/25
+fr_formats.SHORT_DATE_FORMAT = "d/m/y"      # 05/10/25
 
-it_formats.DATE_FORMAT = "d/m/y"            # 25/10/25
-it_formats.DATETIME_FORMAT = "d M Y H:i"    # 25 Ott 2006 14:30
-it_formats.SHORT_DATE_FORMAT = "d/m/y"      # 25/10/25
+it_formats.DATE_FORMAT = "d/m/y"            # 05/10/25
+it_formats.DATETIME_FORMAT = "d M Y H:i"    # 05 Ott 2006 14:30
+it_formats.SHORT_DATE_FORMAT = "d/m/y"      # 05/10/25
+
+nl_formats.DATE_FORMAT = "j-m-y"            # 5-10-25
+nl_formats.DATETIME_FORMAT = "j b Y H:i"    # 5 окт 2006 14:30
+nl_formats.SHORT_DATE_FORMAT = "j-m-y"      # 5-10-25
+
+pl_formats.DATE_FORMAT = "d-m-y"            # 05-10-25
+pl_formats.DATETIME_FORMAT = "j M Y H:i"    # 5 Paź 2006 14:30
+pl_formats.SHORT_DATE_FORMAT = "d-m-y"      # 05-10-25
 
 pt_br_formats.DATE_FORMAT = 'd/m/y'
 pt_br_formats.DATETIME_FORMAT = r"j \d\e M \d\e Y à\s H:i"
 pt_br_formats.SHORT_DATE_FORMAT = 'd/m/y'
+
+ro_formats.DATE_FORMAT = "j m y"             # 5 10 25
+ro_formats.DATETIME_FORMAT = "j b Y, H:i"    # 5 oct 2025, 14:30
+ro_formats.SHORT_DATE_FORMAT = "d.m.y"       # 05.10.25
 
 ru_formats.DATE_FORMAT = 'd.m.y'
 ru_formats.DATETIME_FORMAT = "j M Y H:i:s"
 ru_formats.SHORT_DATE_FORMAT = 'd.m.y'
 
 tr_formats.DATE_FORMAT = 'd m y'            # 05 10 25
-tr_formats.DATETIME_FORMAT = "d M Y H:i"    # 5 Oct 2025 14:30
+tr_formats.DATETIME_FORMAT = "d M Y H:i"    # 5 Eki 2025 14:30
 tr_formats.SHORT_DATE_FORMAT = 'd m y'      # 05 10 25
 
 uk_formats.DATE_FORMAT = 'd.m.y'            # 05.10.25
