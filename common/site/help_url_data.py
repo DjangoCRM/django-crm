9c6bc56392b325d7be1d1b6fc3d8ef0ce9fb1d38
VadKhar <django.crm@outlook.com>
2025-12-25 10:45:41 +0200

    Add help URL mappings for CRM pages

---
+++ common/site/help_url_data.py
@@ -0,0 +1,74 @@
+"""
+This file contains a mapping of CRM pages to their corresponding help web pages.
+"""
+
+
+HELP_URLS = {
+    # CRM home page
+    "en////": "https://djangocrm.github.io/info/help/",
+
+    # Tasks module
+
+    # Memo model
+    # add page
+    "en/tasks/memo/a/": "https://djangocrm.github.io/info/help/creating-crm-note/",
+    # list page
+    "en/tasks/memo/l/": "https://djangocrm.github.io/info/help/notes-crm/",
+    # instance page
+    "en/tasks/memo/i/": "https://djangocrm.github.io/info/help/editing-updating-crm-note/",
+    # delete page
+    "en/tasks/memo/d/": "https://djangocrm.github.io/info/help/deleting-crm-note/",
+
+    # Task model
+    # add page
+    "en/tasks/task/a/": "https://djangocrm.github.io/info/help/creating-assigning-tasks/",
+    # list page
+    "en/tasks/task/l/": "https://djangocrm.github.io/info/help/task-management/",
+    # instance page
+    "en/tasks/task/i/": "https://djangocrm.github.io/info/help/editing-updating-tasks/",
+    # delete page
+    "en/tasks/task/d/": "https://djangocrm.github.io/info/help/deleting-tasks/",
+
+    # Project model
+    # add page
+    "en/tasks/project/a/": "https://djangocrm.github.io/info/help/creating-assigning-projects/",
+    # list page
+    "en/tasks/project/l/": "https://djangocrm.github.io/info/help/project-management/",
+
+    # CRM module
+
+    # Request model
+    # list page
+    "en/crm/request/l/": "https://djangocrm.github.io/info/help/commercial-requests-management/",
+    # instance page
+    "en/crm/request/i/": "https://djangocrm.github.io/info/help/request-processing/",
+
+    # Deal model
+    # list page
+    "en/crm/deal/l/": "https://djangocrm.github.io/info/help/deals-management/",
+    # instance page
+    "en/crm/deal/i/": "https://djangocrm.github.io/info/help/working-with-deals/",
+
+    # Lead model
+    # list page
+    "en/crm/lead/l/": "https://djangocrm.github.io/info/help/lead-management/",
+
+    # Company model
+    # list page
+    "en/crm/company/l/": "https://djangocrm.github.io/info/help/company-management/",
+
+    # Contact model
+    # list page
+    "en/crm/contact/l/": "https://djangocrm.github.io/info/help/contact-management/",
+
+    # Mass mail Module
+
+    # Mailingout model
+    # list page
+    "en/massmail/mailingout/l/": "https://djangocrm.github.io/info/help/managing-mass-mailings/",
+
+    # Emlmessage model
+    # list page
+    "en/massmail/emlmessage/l/": "https://djangocrm.github.io/info/help/managing-newsletters/",
+
+}
