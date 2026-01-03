9f552067db61a98b29479109386f83819173ee37
VadKhar <VadKhar@outlook.com>
2026-01-03 12:56:42 +0200

    refactor: update Quality app configuration with new name and label

--- quality/apps.py
+++ quality/apps.py
@@ -4,4 +4,6 @@ from django.utils.translation import gettext_lazy as _
 
 class QualityConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
-    name = _('Transaction quality')
+    name = 'quality'
+    verbose_name = _('Transaction quality')
+    label = 'quality'
