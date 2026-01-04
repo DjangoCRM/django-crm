4e782ab85217c63bd73b21aaa72a96d814912962
VadKhar <VadKhar@outlook.com>
2026-01-04 18:05:14 +0200

    Add admin for Quality app

---
+++ quality/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+from crm.admin import crm_site
+from crm.admin import TranslateNameModelAdmin
+from quality.models import TransactionQualitySignal
+from quality.models import TransactionQualityEvent
+
+
+class TransactionQualitySignalAdmin(TranslateNameModelAdmin):
+    list_display = ('name', 'weight', 'notes')
+
+
+class TransactionQualityEventAdmin(TranslateNameModelAdmin):
+    list_display = ('signal', 'weight', 'details', )
+
+
+
+admin.site.register(TransactionQualitySignal,
+    TransactionQualitySignalAdmin
+)
+admin.site.register(TransactionQualityEvent,
+    TransactionQualityEventAdmin
+)
+
+crm_site.register(
+    TransactionQualitySignal,
+    TransactionQualitySignalAdmin
+)
+crm_site.register(
+    TransactionQualityEvent,
+    TransactionQualityEventAdmin
+)
